a095d847f9c5d12f10956394e509f80da616f5a4
Rômulo Souza <romulovinicius10@gmail.com>
2019-04-16 15:05:18 -0300

    Config keyboard for ship

--- game.py
+++ game.py
@@ -17,27 +17,39 @@ shot = pygame.image.load('images/laser.png')
 
 
 class Game:
+    def __init__(self):
+        self.pos_x = screen.get_width()/2
+
     def run(self):
         shoot_y = 0
         while True:
             clock.tick(60)
             screen.fill((0, 0, 0))
-            x, y = pygame.mouse.get_pos()
             screen.blit(background, [0, 0])
-            screen.blit(ship, (x-ship.get_width()/2, ship_top))
+            screen.blit(ship, (self.pos_x-ship.get_width()/2, ship_top))
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
                 elif event.type == MOUSEBUTTONDOWN:
                     shoot_y = 500
-                    shoot_x = x
+                    shoot_x = self.pos_x
 
+            key = pygame.key.get_pressed()
+            self.keyboard_manager(key)
+            
             if shoot_y > 0:
                 screen.blit(shot, (shoot_x, shoot_y))
                 shoot_y -= 10
 
             pygame.display.update()
-
+    
+    def keyboard_manager(self, key):
+        if key[pygame.K_ESCAPE]:
+            sys.exit()
+        elif key[pygame.K_LEFT]:
+            self.pos_x -= 3
+        elif key[pygame.K_RIGHT]:
+            self.pos_x += 3
 
 if __name__ == '__main__':
     game = Game()
