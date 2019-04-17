1d345259f43c9d4bb9e0e835a2711a50b400f5e4
Rômulo Souza <romulovinicius10@gmail.com>
2019-04-17 00:00:18 -0300

    Fix shot logic

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 import pygame, sys
 from pygame.locals import *
+from collections import deque
 
 clock = pygame.time.Clock()
 screen = pygame.display.set_mode((800, 600))
@@ -14,6 +15,22 @@ screen.blit(ship, (ship_left, ship_top))
 
 background = pygame.image.load('images/background.jpg')
 shot = pygame.image.load('images/laser.png')
+# node_1 = pygame.image.load('images/number1.png')
+# node_3 = pygame.image.load('images/number3.png')
+# node_5 = pygame.image.load('images/coin.png')
+
+# class Node:
+#     def __init__(self, value):
+#         self.value = value
+#         if value == 1:
+#             self.image = node_1
+#         elif value == 3:
+#             self.image = node_3
+#         else: self.image = node_5
+
+#     def draw_node(self, pos_x, pos_y):
+#         screen.blit(self.image, (pos_x, pos_y))
+
 
 class Shot:
     def __init__(self):
@@ -21,15 +38,21 @@ class Shot:
         self.pos_x = 0
         self.pos_y = 500
 
+    def draw_shot(self):
+        screen.blit(self.image, (self.pos_x,self.pos_y))
+
 class Game:
     def __init__(self):
         self.pos_x = screen.get_width()/2 # ship horizontal position
 
     def run(self):
-        shoot_y = 0
-        shot_list = []
+        shot_list = deque([])
+
+        # node = Node(5)
+
         while True:
             clock.tick(60)
+            # node.draw_node(self.pos_x, 2*ship_top)
             # screen.fill((0, 0, 0))
             screen.blit(background, [0, 0])
             screen.blit(ship, (self.pos_x-ship.get_width()/2, ship_top))
@@ -45,8 +68,12 @@ class Game:
             self.keyboard_manager(key)
 
             for i in shot_list:
-                screen.blit(i.image, (i.pos_x,i.pos_y))
+                i.draw_shot()
                 i.pos_y -= 10
+            
+            if shot_list and shot_list[0].pos_y <= 0:
+                shot_list.popleft()
+
 
             pygame.display.update()
     
