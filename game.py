fb525a83a036cd85ef5397bea2aaece0a7577d04
Rômulo Souza <romulovinicius10@gmail.com>
2019-04-19 04:22:41 -0300

    Add scketch of dijkstra with heap

--- game.py
+++ game.py
@@ -1,8 +1,10 @@
 import pygame, sys
 from pygame.locals import *
 from collections import deque
+from collections import defaultdict
 from os.path import abspath, dirname
 from random import choice, randint
+import heapq
 
 
 
@@ -29,23 +31,6 @@ posto = pygame.image.load('images/mystery.png')
 portal = pygame.image.load('images/enemy2_1.png')
 background = pygame.image.load('images/space.jpg')
 shot = pygame.image.load('images/laser.png')
-# node_1 = pygame.image.load('images/number1.png')
-# node_3 = pygame.image.load('images/number3.png')
-# node_5 = pygame.image.load('images/coin.png')
-
-# class Node:
-#     def __init__(self, value):
-#         self.value = value
-#         if value == 1:
-#             self.image = node_1
-#         elif value == 3:
-#             self.image = node_3
-#         else: self.image = node_5
-
-#     def draw_node(self, pos_x, pos_y):
-#         screen.blit(self.image, (pos_x, pos_y))
-
-
 
 
 class Shot:
@@ -59,18 +44,21 @@ class Shot:
 
 
 class Map:
-    def __init__(self, pos_x_ini, pos_x_end, pos_y_ini, pos_y_end):
+    def __init__(self, id, pos_x_ini, pos_x_end, pos_y_ini, pos_y_end):
+        self.id = id
         self.pos_x_ini = pos_x_ini
         self.pos_x_end = pos_x_end
         self.pos_y_ini = pos_y_ini
         self.pos_y_end = pos_y_end
-        self.coust = randint(1, 11)
+        self.cost = randint(1, 11)
         if(pos_x_ini == 1060 and pos_y_ini == 0):
-            self.coust = 'x' # posicao final
+            self.cost = 'x' # posicao final
         if(pos_x_ini == 0 and pos_y_ini == 588):
-            self.coust = 0 # posicao inicial
+            self.cost = 0 # posicao inicial
         self.has_ipiranga = randint(0, 2)
         self.neighbors = []
+        self.lowest_cost = None
+        self.visited = False
 
     def add_node(self, value):
         self.nodes.add(value)
@@ -81,37 +69,93 @@ class Map:
         self.distances[(from_node, to_node)] = distance
 
     def __str__(self):
-        return str(self.pos_x_ini) + " " + str(self.pos_x_end) + " " + str(self.pos_y_ini) + " " + str(self.pos_y_end) + " " + str(self.coust) + " " + str(self.has_ipiranga)
+        return str(self.pos_x_ini) + " " + str(self.pos_x_end) + " " + str(self.pos_y_ini) + " " + str(self.pos_y_end) + " " + str(self.cost) + " " + str(self.has_ipiranga)
 
 
-def dijsktra(graph, initial):
-  visited = {initial: 0}
-  path = {}
+def dijsktra(graph):
+    # id, origin, cost
+    nodes_cost = defaultdict(list)
+    to_visit = []
+    # nodes_cost['2'].append((1, 2))
 
-  nodes = set(graph.nodes)
+    first = graph[0]
+    first.visited = True
+    first.lowest_cost = 0
+    # print(graph[0].cost)
+    # print(graph[0].neighbors[0].cost)
+    # print(graph[0].neighbors[1].cost)
+    
+    # for i in nodes_cost: print (i)
+    for i in first.neighbors:
+        cost = i.cost + first.lowest_cost
+        found = False
+        for j in to_visit:
+            # print('j = ' + str(j) + ' i.id = ' + str(i.id))
+            if i.id == j[2] and cost < j[0]:
+                j = (cost, first.id, i.id)
+                heapq.heapify(to_visit)
+                found = True
+        
+        # cost, origin, destin
+        if not found:
+            heapq.heappush(to_visit, (cost, first.id, i.id))
 
-  while nodes: 
-    min_node = None
-    for node in nodes:
-      if node in visited:
-        if min_node is None:
-          min_node = node
-        elif visited[node] < visited[min_node]:
-          min_node = node
+    print(to_visit)
+    while not to_visit: # preso no loop
+        print('###')
+        to_verify = heapq.heappop(to_visit)
+        id = str(to_verify[2])
+        origin = to_verify[1]
+        cost = to_verify[0]
+        
+        # verifica se existe o peso para chegar no nó e atualiza
+        if nodes_cost[id]:
+            if  cost < nodes_cost[id][1]:
+                nodes_cost[id] = (origin, cost)
+        else:
+            nodes_cost[id] = (origin, cost)
+            # print(nodes_cost[id])
 
-    if min_node is None:
-      break
+        node = [x for x in graph if (x.id == to_verify[2])][0]
+        node.visited = True
+        # print('####')
+        # print(nodes_cost[id][0])
+        node_cost = nodes_cost[id][1]
+        for i in node.neighbors:
+            cost = node_cost + i.cost
+            if not i.visited:
+                # print('entrous')
+                heapq.heappush(to_visit, (cost, node.id, i.id))
+        # print('aqui ' + str(node))
+        # print(node.id)
+        # print(to_verify[2])
+        # print('#####')
+            
 
-    nodes.remove(min_node)
-    current_weight = visited[min_node]
+    # for i in first.neighbors:
+        # print(nodes_cost)
+        # print(nodes_cost[str(i.id)])
 
-    for edge in graph.edges[min_node]:
-      weight = current_weight + graph.distance[(min_node, edge)]
-      if edge not in visited or weight < visited[edge]:
-        visited[edge] = weight
-        path[edge] = min_node
+        # id = str(i.id)
+        # print(id)
+        # print(i.cost)
+        # total_cost = first.lowest_cost + i.cost
+        # if nodes_cost[id]:
+        #     if  total_cost < nodes_cost[id][0][1]:
+        #         nodes_cost[id] = (first.id, total_cost)
+        # else:
+        #     nodes_cost[id] = (first.id, total_cost)
+        #     print(nodes_cost[id])
+        # print("####")
+        
 
-  return visited, path
+    # first.neighbors[0].lowest_cost = (first.neighbors.id,)
+    
+
+
+    # for i in graph: print(i.id)
+
+    # return visited, path
 
 def create_edges(mapa):
     for i in mapa:
@@ -122,7 +166,7 @@ def create_edges(mapa):
 
         if(up):
             i.neighbors.append(up[0])
-        if(down):
+        if(down):   
             i.neighbors.append(down[0])
         if(right):
             i.neighbors.append(right[0])
@@ -132,9 +176,13 @@ def create_edges(mapa):
 
 def create_map():
     map = []
-    for i in range(0, 1277, 212):
-        for j in range(0, 689, 98):
-            aux = Map(i, i+211, j, j+97)
+    id = 1
+    for i in range(0, 1067, 212):
+        # print(i)
+        for j in range(593, 0, -98):
+            # print(j)
+            aux = Map(id, i, i+211, j, j+97)
+            id += 1
             map.append(aux)
     return map
 
@@ -157,12 +205,13 @@ class Game:
         self.pos_x = 100 # screen.get_width()/2 # ship horizontal position
         self.pos_y = ship_top
         self.combustivel = 50
-        self.onde_eu_to = None
+        self.my_pos = None
         self.ativar_postos = True
 
     def run(self):
         shot_list = deque([])
-
+        dijsktra(self.map)
+        # print(self.map[0])
         # node = Node(5)
 
         while True:
@@ -179,7 +228,7 @@ class Game:
 
             # screen.blit(portal, (900, 25))
 
-            self.draw_cousts()
+            self.draw_costs()
             self.check_colisor()
 
             for event in pygame.event.get():
@@ -219,7 +268,7 @@ class Game:
             self.ativar_postos = True
 
     
-    def draw_cousts(self):
+    def draw_costs(self):
         largeText = pygame.font.Font('freesansbold.ttf',25)
         TextSurf, TextRect = text_objects(str(self.combustivel), largeText)
         TextRect.center = ((30),(28))
@@ -227,23 +276,23 @@ class Game:
         for i in self.map:
             if(self.ativar_postos and i.has_ipiranga == 1):
                 screen.blit(posto, ((i.pos_x_ini),(i.pos_y_ini)))
-            TextSurf, TextRect = text_objects(str(i.coust), largeText)
+            TextSurf, TextRect = text_objects(str(i.cost), largeText)
             TextRect.center = ((i.pos_x_ini+105),(i.pos_y_ini+48))
             gameDisplay.blit(TextSurf, TextRect)
 
     def check_colisor(self):
         for i in self.map:
-            if(self.onde_eu_to != i and
+            if(self.my_pos != i and
                  self.pos_x >= i.pos_x_ini and 
                  self.pos_x < i.pos_x_end and 
                  self.pos_y >= i.pos_y_ini and 
                  self.pos_y < i.pos_y_end):
-                if(i.coust == 'x'):
+                if(i.cost == 'x'):
                     print('YOU WIN')
                     sys.exit()
-                # print('funciona msm ' + str(i.coust))
-                self.onde_eu_to = i
-                self.combustivel -= i.coust
+                # print('funciona msm ' + str(i.cost))
+                self.my_pos = i
+                self.combustivel -= i.cost
                 if(self.ativar_postos and i.has_ipiranga == 1):
                     i.has_ipiranga = 0
                     self.combustivel += 5
